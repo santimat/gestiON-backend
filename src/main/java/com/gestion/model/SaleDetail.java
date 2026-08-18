9f21df75f8f336bb6f69a10543079eaab1d1e7b9
santimat <santimaturo1@gmail.com>
2026-08-18 19:57:54 -0300

    start with sale details model

---
+++ src/main/java/com/gestion/model/SaleDetail.java
@@ -0,0 +1,23 @@
+package com.gestion.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "sale_details")
+public class SaleDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "sale_id")
+    private Sale sale;
+
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private Float price;
+}
