aeca7b8f8a028cbdd61e581a38dc82011d5cf0ab
santimat <santimaturo1@gmail.com>
2026-08-18 19:06:10 -0300

    add Category and User models

---
+++ src/main/java/com/gestion/model/Category.java
@@ -0,0 +1,21 @@
+package com.gestion.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false, length = 50)
+    private String name;
+}
---
+++ src/main/java/com/gestion/model/User.java
@@ -0,0 +1,43 @@
+package com.gestion.model;
+
+import com.gestion.enums.Role;
+import com.gestion.enums.UserStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "commerce_id")
+    private Commerce commerce;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+}
