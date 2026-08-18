cfd64fd4743422c438791e9abc8560a0af128e62
santimat <santimaturo1@gmail.com>
2026-08-18 18:04:04 -0300

    add Commerce model

--- src/main/java/com/gestion/model/Commerce.java
+++ src/main/java/com/gestion/model/Commerce.java
@@ -1,13 +1,34 @@
 package com.gestion.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "commerces")
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Commerce {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
+    private String businessName;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String address;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String phoneNumber;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String cuit;
+
+    @Column(unique = true)
+    private String logoUrl;
 }
