8c30bb4566758b499ffebe8f8b26c5a08f924a4b
santimat <santimaturo1@gmail.com>
2026-08-21 13:17:55 -0300

    Auth service and UserPostController are in process

--- src/main/java/com/gestion/controller/user/UserPostController.java
+++ src/main/java/com/gestion/controller/user/UserPostController.java
@@ -2,9 +2,10 @@ package com.gestion.controller.user;
 
 import com.gestion.dto.request.user.UserRequest;
 import com.gestion.dto.response.user.UserResponse;
-import com.gestion.model.User;
+import com.gestion.mappers.UserMapper;
 import com.gestion.service.user.UserCreatorService;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,18 +14,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/-----") // a completar con lo correcto
+@RequestMapping("/api/users")
+@AllArgsConstructor
 public class UserPostController {
     private final UserCreatorService userCreatorService;
-
-    public UserPostController(UserCreatorService userCreatorService){
-        this.userCreatorService = userCreatorService;
-    }
-
+    private final UserMapper userMapper;
 
     @PostMapping
-    public ResponseEntity<UserResponse> create(@Valid @RequestBody UserRequest request){
-        User user = userCreatorService.create(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(UserResponse.toResponse(user));
+    public ResponseEntity<UserResponse> create(@Valid @RequestBody UserRequest request) {
+        UserResponse response = userMapper.toResponse(userCreatorService.createUser(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
--- src/main/java/com/gestion/service/auth/AuthRegisterService.java
+++ src/main/java/com/gestion/service/auth/AuthRegisterService.java
@@ -8,6 +8,4 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class AuthRegisterService {
     private final PasswordEncoder passwordEncoder;
-
-    
 }
