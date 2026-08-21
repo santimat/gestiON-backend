d9258fc6cccd14014aa59bbde87ba5a6e286d035
santimat <santimaturo1@gmail.com>
2026-08-21 15:34:49 -0300

    improve method to check existing resources

--- src/main/java/com/gestion/repository/JpaCommerceRepository.java
+++ src/main/java/com/gestion/repository/JpaCommerceRepository.java
@@ -3,8 +3,6 @@ package com.gestion.repository;
 import com.gestion.model.Commerce;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface JpaCommerceRepository extends JpaRepository<Commerce, Long> {
-    Optional<Commerce> findByBusinessName(String businessName);
+    boolean existsByCuit(String cuit);
 }
--- src/main/java/com/gestion/repository/JpaUserRepository.java
+++ src/main/java/com/gestion/repository/JpaUserRepository.java
@@ -3,9 +3,7 @@ package com.gestion.repository;
 import com.gestion.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface JpaUserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
