4768769626fc704deeb0dd223c7a8bc2adad3c77
santimat <santimaturo1@gmail.com>
2026-08-21 15:34:56 -0300

    add new exceptions

--- src/main/java/com/gestion/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/gestion/exception/handler/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
-package com.gestion.exception;
+package com.gestion.exception.handler;
 
+import com.gestion.exception.DuplicateResourceException;
+import com.gestion.exception.FileManagerException;
+import com.gestion.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,6 +20,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DuplicateResourceException.class)
     public ResponseEntity<String> handleDuplicateResourceException(DuplicateResourceException exception) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(FileManagerException.class)
+    public ResponseEntity<String> handleFileManagerException(FileManagerException exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
     }
 
     // Caso general para cualquier otra excepción no manejada
