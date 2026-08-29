de911f0d03b28bfa0ffd7083bbfa48ddb5b476ff
santimat <santimaturo1@gmail.com>
2026-08-29 00:36:20 -0300

    comment some stuff which is no currently needed

--- src/main/java/com/gestion/controller/product/ProductGetController.java
+++ src/main/java/com/gestion/controller/product/ProductGetController.java
@@ -1,27 +1,27 @@
-package com.gestion.controller.product;
-
-import com.gestion.dto.response.product.ProductResponse;
-import com.gestion.mappers.ProductMapper;
-import com.gestion.model.Product;
-import com.gestion.service.product.ProductFinderByIdService;
-import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/products")
-@AllArgsConstructor
-public class ProductGetController {
-    private final ProductFinderByIdService productFinderByIdService;
-    private final ProductMapper productMapper;
-
-    @GetMapping("/{id}")
-    public ResponseEntity<ProductResponse> getById(@PathVariable Long id) {
-        Product product = productFinderByIdService.findBy(id);
-        ProductResponse response = productMapper.toResponse(product);
-        return ResponseEntity.ok(response);
-    }
-}
+//package com.gestion.controller.product;
+//
+//import com.gestion.dto.response.product.ProductResponse;
+//import com.gestion.mappers.ProductMapper;
+//import com.gestion.model.Product;
+//import com.gestion.service.product.ProductFinderByIdService;
+//import lombok.AllArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/products")
+//@AllArgsConstructor
+//public class ProductGetController {
+//    private final ProductFinderByIdService productFinderByIdService;
+//    private final ProductMapper productMapper;
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<ProductResponse> getById(@PathVariable Long id) {
+//        Product product = productFinderByIdService.findBy(id);
+//        ProductResponse response = productMapper.toResponse(product);
+//        return ResponseEntity.ok(response);
+//    }
+//}
--- src/main/java/com/gestion/service/user/UserGetAllService.java
+++ src/main/java/com/gestion/service/user/UserGetAllService.java
@@ -1,19 +1,12 @@
 package com.gestion.service.user;
 
-import com.gestion.model.User;
-import com.gestion.repository.JpaUserRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-
-@Service
-@AllArgsConstructor
-public class UserGetAllService {
-    private final JpaUserRepository userRepository;
-
-    // TODO: agregar query que acepte filtros de busqueda (hay que hacer la query en el repository)
-    public Page<User> findAll(Pageable pageable) {
-        return userRepository.findAll(pageable);
-    }
-}
+//@Service
+//@AllArgsConstructor
+//public class UserGetAllService {
+//    private final JpaUserRepository userRepository;
+//
+//    // TODO: agregar query que acepte filtros de busqueda (hay que hacer la query en el repository)
+//    public Page<User> findAll(Pageable pageable) {
+//        return userRepository.findAll(pageable);
+//    }
+//}
