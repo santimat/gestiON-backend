413c64100580127595c1d98af1e5311688de9278
santimat <santimaturo1@gmail.com>
2026-08-29 00:37:52 -0300

    remove logo (temporally)

--- src/main/java/com/gestion/dto/request/commerce/CommerceRequest.java
+++ src/main/java/com/gestion/dto/request/commerce/CommerceRequest.java
@@ -1,13 +1,10 @@
 package com.gestion.dto.request.commerce;
 
 
-import org.springframework.web.multipart.MultipartFile;
-
 public record CommerceRequest(
         String businessName,
         String address,
         String phoneNumber,
-        String cuit,
-        MultipartFile logo
+        String cuit
 ) {
 }
